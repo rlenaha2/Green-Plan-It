29f3eac86eba6b6a5c4b99599ca27ddef0ff1a3d
Ryan <rlenaha2@gmail.com>
2017-10-23 13:14:24 -0700

    Updated model to inclue pipe object

--- model.py
+++ model.py
@@ -5,27 +5,56 @@ import numpy as np
 
 
 
+def column_index(df, query_cols):
+    """
+    Creates indecies from column names to be used in OneHotEncoder
+    Input 
+    ------- 
+    Dataframe with all columns
+    query_cols columns which indicies will be returned
+
+    Output
+    -------
+    Indicies of columns from input
+    """
+
+
+    cols = df.columns.values
+    sidx = np.argsort(cols)
+    return sidx[np.searchsorted(cols,query_cols,sorter=sidx)]
+
+
 def create_model(X, y):
-'''
-Creates a Ridge Model
-Inputs
---------
-X: A dataframe with all indicator variables transformed into numerical positive values
-y: reported energy values
+    '''
+    Creates a Ridge Model
+    Inputs
+    --------
+    X: A dataframe with all indicator variables transformed into numerical positive values
+    y: reported energy values
 
-Outputs
---------
+    Outputs
+    --------
 
 
-'''
+    '''
+    
+    col_dummies_index = column_index(X, ['DIVISION','REPORTABLE_DOMAIN',
+               'TYPEHUQ','Climate_Region_Pub',
+               'AIA_Zone','CONDCOOP','CONVERSION','WALLTYPE','ROOFTYPE',
+               'STOVENFUEL','STOVEFUEL','OVENFUEL', 'OVENUSE','AMTMICRO',
+               'OUTGRILLFUEL', 'NUMMEAL','FUELFOOD', 'TVTYPE1','PCTYPE1',
+               'EQUIPM', 'FUELHEAT', 'NGFPFLUE','USENGFP','DIFFUEL','EQMAMT',
+               'H2OTYPE1', 'FUELH2O', 'COOLTYPE', 'FUELPOOL','FUELTUB','TYPEGLASS',
+               'ADQINSUL','DRAFTY'])
 
     X_train, X_test, y_train, y_test = train_test_split(
     X, y, random_state = 52)
 
-    ridge_model = Ridge(alpha = 3)
-    ridge_model.fit(X_train, np.log(y_train))
-    energy_predictions = ridge_model.predict(X_test)
+    pipe = Pipeline([('one_hot_encoder', OneHotEncoder(categorical_features=
+                                                           col_dummies_index)),
+               ('ridge_model', Ridge(alpha=3))])
+    pipe.fit(X_train, y = y_train)
 
-    return ridge_model
+    return pipe 
 
 
