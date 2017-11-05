ccc96dc65b95a0b9a52c8edfbdd6fdbe0f36ce92
Ryan <rlenaha2@gmail.com>
2017-11-04 21:27:45 -0700

    Updated model file for website to be consistent with pep8 and remove plots

--- make_prediction.py
+++ make_prediction.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as pd 
+import pandas as pd
 
 
 def read_df_pred(filepath):
@@ -7,16 +7,17 @@ def read_df_pred(filepath):
     Creates a pandas dataframe from the input csv
     Input
     -------
-    Path to the csv 
+    Path to the csv
 
-    Output  
+    Output
     -------
-    Pandas dataframe 
+    Pandas dataframe
     """
 
     df_pred = pd.read_csv(filepath)
 
     return df_pred
+
 
 def clean_df_pred(df_pred):
     """
@@ -24,43 +25,34 @@ def clean_df_pred(df_pred):
     Input
     -------
     df from input csv
- 
-    Output 
-    ------- 
+
+    Output
+    -------
     Cleaned Dataframe
     """
 
     df_pred = df_pred.T
-    new_header = df_pred.iloc[0] 
-    df_pred = df_pred[1:] 
+    new_header = df_pred.iloc[0]
+    df_pred = df_pred[1:]
     df_pred.columns = new_header
     df_pred = df_pred.iloc[[3]]
-   
+
     return df_pred
- 
+
+
 def make_prediction(pipe, df_pred):
-    '''
+    """
     Function to create predicitons based on user input for a new house.
     Inputs
     --------
     Pipe object which contains predicitive model
-    Cleaned dataframe which contains instance to be predicted 
-    
+    Cleaned dataframe which contains instance to be predicted
+
     Outputs
     --------
     Yearly energy usage in BTU
-    '''
+    """
 
     energy_prediction = pipe.predict(df_pred)
-#    energy_prediction = np.exp(energy_prediction)
 
     return energy_prediction
-
-
-
-
-
-
-
-
-
--- website/model.py
+++ website/model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import HuberRegressor 
+from sklearn.linear_model import HuberRegressor
 import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
@@ -8,71 +8,71 @@ import pickle
 from preprocessing import clean_df
 from preprocessing import create_target
 from preprocessing import create_feature_dataframe
+from make_prediction import make_prediction
 
 
 def column_index(df, query_cols):
     """
     Creates indecies from column names to be used in OneHotEncoder
-    Input 
-    ------- 
-    Dataframe with all columns
-    query_cols columns which indicies will be returned
+
+    Input
+    -------
+    df: Dataframe with all columns
+    query_cols: columns which indicies will be returned
 
     Output
     -------
-    Indicies of columns from input
+    Indicies of columns from input Dataframe
     """
-
 
     cols = df.columns.values
     sidx = np.argsort(cols)
-    return sidx[np.searchsorted(cols,query_cols,sorter=sidx)]
+    return sidx[np.searchsorted(cols, query_cols, sorter=sidx)]
 
 
 def create_model(X, y):
-    '''
-    Creates a Ridge Model
+    """
+    Creates a Linear Model minimizing the Huber loss
+
     Inputs
     --------
-    X: A dataframe with all indicator variables transformed into numerical positive values
-    y: reported energy values
+    X_train: A dataframe with all indicator variables transformed
+             into numerical positive values
+    y_train: reported energy values
 
     Outputs
     --------
+    Pipe object that can be used to make predictions
+    Also created a pickle of the pipe object in the current directory
+    """
+
+    col_index = column_index(X, ['DIVISION', 'REPORTABLE_DOMAIN',
+                                 'TYPEHUQ', 'Climate_Region_Pub',
+                                 'AIA_Zone', 'CONDCOOP', 'CONVERSION',
+                                 'WALLTYPE', 'ROOFTYPE', 'STOVENFUEL',
+                                 'STOVEFUEL', 'OVENFUEL', 'OVENUSE',
+                                 'AMTMICRO', 'OUTGRILLFUEL', 'NUMMEAL',
+                                 'FUELFOOD', 'TVTYPE1', 'PCTYPE1',
+                                 'EQUIPM', 'FUELHEAT', 'NGFPFLUE',
+                                 'USENGFP', 'DIFFUEL', 'EQMAMT',
+                                 'H2OTYPE1', 'FUELH2O', 'COOLTYPE',
+                                 'FUELPOOL', 'FUELTUB', 'TYPEGLASS',
+                                 'ADQINSUL', 'DRAFTY'])
+
+    pipe = Pipeline([('one_hot_encoder', OneHotEncoder(
+                                         categorical_features=col_index)),
+                     ('huber_model', HuberRegressor())])
+    pipe.fit(X, y=np.sqrt(y))
+
+    with open('pipe_model.p', 'wb') as f:
+        pickle.dump(pipe, f)
+
+    return pipe
 
 
-    '''
-    
-    col_dummies_index = column_index(X, ['DIVISION','REPORTABLE_DOMAIN',
-               'TYPEHUQ','Climate_Region_Pub',
-               'AIA_Zone','CONDCOOP','CONVERSION','WALLTYPE','ROOFTYPE',
-               'STOVENFUEL','STOVEFUEL','OVENFUEL', 'OVENUSE','AMTMICRO',
-               'OUTGRILLFUEL', 'NUMMEAL','FUELFOOD', 'TVTYPE1','PCTYPE1',
-               'EQUIPM', 'FUELHEAT', 'NGFPFLUE','USENGFP','DIFFUEL','EQMAMT',
-               'H2OTYPE1', 'FUELH2O', 'COOLTYPE', 'FUELPOOL','FUELTUB','TYPEGLASS',
-               'ADQINSUL','DRAFTY'])
-
-#    X_train, X_test, y_train, y_test = train_test_split(
-#    X, y, random_state = 52)
-
-    pipe = Pipeline([('one_hot_encoder', OneHotEncoder(categorical_features=
-                                                           col_dummies_index)),
-               ('ridge_model', HuberRegressor())])
-    pipe.fit(X, y = np.sqrt(y))
-    
-    with open('pipe_model.p', 'wb') as f:  
-        pickle.dump(pipe, f, protocol=2)
-
-
-
-    return pipe 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     df = pd.read_csv("recs2009_public.csv")
     df = clean_df(df)
     y = create_target(df)
     X = create_feature_dataframe(df)
-    pipe = create_model(X,y)
-
-
+    pipe = create_model(X, y)
